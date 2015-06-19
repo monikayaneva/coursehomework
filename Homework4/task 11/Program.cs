d2290524fbffb885e0a5a2cba3c9491d403f724b
monikayaneva <monika.ianeva@gmail.com>
2015-06-20 02:09:55 +0300

    Homework4

---
+++ Homework4/task 11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a card");
+            string card =(Console.ReadLine());
+            switch (card)
+            {
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "10":
+                case "J":
+                case "Q":
+                case "K":
+                case "A":
+                    Console.WriteLine("Valid card!");
+                    break;
+                default:
+                    Console.WriteLine("Invalid card!");
+                    break;
+            }
+
+        }
+    }
+}
