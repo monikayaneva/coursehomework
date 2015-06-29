7dc05d182bff7a09a25e12e29c156f69e37cb390
monikayaneva <monika.ianeva@gmail.com>
2015-06-29 15:24:48 +0300

    Homework6

---
+++ Homework6/task 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = { 3, 6, 7, 12, 22, 5 };
+           int sum = 0;
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (Array[i] % 2 == 0)
+                    sum = Array[i] + sum;   
+                } 
+            Console.WriteLine(sum);
+            }
+        }
+    }
+
---
+++ Homework6/task 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter how to long array:");
+            long n = long.Parse(Console.ReadLine());
+            long[] Array = new long[n];
+            Array[0] = 1;
+            Array[1] = 1;
+
+            for (int i = 2; i < Array.Length; i++)
+            {
+                Array[i] = Array[i - 1] +  Array[i - 2];
+
+            }
+            foreach  (long element in Array)
+            {
+                Console.WriteLine();
+                Console.WriteLine(element);  
+            }
+        }
+    }
+}
